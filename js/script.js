aa3d364809a8df2d5853b630c85628be8770af39
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-03-11 12:27:34 +0100

    Add css and style hovered-color

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById('provinceMap');
 var context = canvas.getContext('2d');
 
 var map = new Image();
+map.crossOrigin = 'anonymous';
 map.src = 'map/provinces.bmp';
 map.onload = function() {
 	context.drawImage(map, 0, 0);
@@ -14,16 +15,15 @@ var selectedColor = document.getElementById('selected-color');
 
 
 function pick(event, destination) {
-  var x = event.layerX;
-  var y = event.layerY;
-  var pixel = context.getImageData(x, y, 1, 1);
-  var data = pixel.data;
+	var x = event.layerX;
+	var y = event.layerY;
+	var pixel = context.getImageData(x, y, 1, 1);
+	var data = pixel.data;
 
-	const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-	destination.style.background = rgba;
-	destination.textContent = rgba;
+	const rgb = `rgb(${data[0]}, ${data[1]}, ${data[2]})`;
+	destination.textContent = rgb;
 
-	return rgba;
+	return rgb;
 }
 
 canvas.addEventListener('mousemove', function(event) {
