d6b90d88e7f950297fb47cad1fc6fc3bcd66e18e
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-03-11 11:43:11 +0100

    Color picker

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
-function drawMap() {
-	var context = document.getElementById('provinceMap').getContext('2d');
+var canvas = document.getElementById('provinceMap');
+var context = canvas.getContext('2d');
 
-	var map = new Image();
-	map.src = 'map/provinces.bmp';
-	map.onload = function() {
-		context.drawImage(map, 0, 0);
-	}
+var map = new Image();
+map.src = 'map/provinces.bmp';
+map.onload = function() {
+	context.drawImage(map, 0, 0);
 }
 
-drawMap();
+var mapData = context.createImageData(5616, 2160);
+
+var hoveredColor = document.getElementById('hovered-color');
+var selectedColor = document.getElementById('selected-color');
+
+
+function pick(event, destination) {
+  var x = event.layerX;
+  var y = event.layerY;
+  var pixel = context.getImageData(x, y, 1, 1);
+  var data = pixel.data;
+
+	const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+	destination.style.background = rgba;
+	destination.textContent = rgba;
+
+	return rgba;
+}
+
+canvas.addEventListener('mousemove', function(event) {
+	pick(event, hoveredColor);
+});
+canvas.addEventListener('click', function(event) {
+	pick(event, selectedColor);
+});
