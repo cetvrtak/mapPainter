fd9ffc32b6f5ef417e826fb3a6b88929bc7d65d5
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-03-13 07:57:35 +0100

    Add province definitions

--- js/script.js
+++ js/script.js
@@ -4,11 +4,6 @@ var context = canvas.getContext('2d');
 var map = new Image();
 map.crossOrigin = 'anonymous';
 map.src = 'map/provinces.bmp';
-map.onload = function() {
-	context.drawImage(map, 0, 0);
-}
-
-var mapData = context.createImageData(5616, 2160);
 
 var hoveredColor = document.getElementById('hovered-color');
 var selectedColor = document.getElementById('selected-color');
@@ -32,3 +27,27 @@ canvas.addEventListener('mousemove', function(event) {
 canvas.addEventListener('click', function(event) {
 	pick(event, selectedColor);
 });
+
+var country = {
+	color: [0,0,0]
+}
+
+function cede(provId, tag) {
+	context.drawImage(map, 0, 0);
+	const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+	const data = imageData.data;
+
+	[red, green, blue] = getProvince(provId).color;
+
+	for (var i = 0; i < data.length; i += 4) {
+		if (data[i] == red && data[i + 1] == green && data[i + 2] == blue)
+		{
+			data[i] = tag[0];
+			data[i + 1] = tag[1];
+			data[i + 2] = tag[2];
+		}
+	}
+	context.putImageData(imageData, 0, 0);
+}
+
+cede(1, country.color);
